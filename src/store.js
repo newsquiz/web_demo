dbe0d73a2275487a90631981736bd9a4e6a97c5f
lanpn <phan.ngoclan58@gmail.com>
2019-07-13 08:56:42 +0700

    Add home

--- src/store.js
+++ src/store.js
@@ -5,7 +5,10 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-
+    user: {
+      name: 'John Smith',
+      imageUrl: 'http://extendthemes.com/demos/mesmerize-pro-furniture/wp-content/uploads/sites/9/2018/06/cropped-pexels-photo-839011.png'
+    }
   },
   mutations: {
 
